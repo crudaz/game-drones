20accb7256aa52ccd5f5c26fb5ff5bfcbd165602
crudaz <crudaz@IVOMac.local>
2018-12-17 21:23:39 -0500

    feat: main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartGameComponent } from './start-game/start-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartGameComponent
   ],
   imports: [
     BrowserModule,
