5793ca99a49bb493450c90bfa20f0e444a6e2643
crudaz <crudaz@IVOMac.local>
2018-12-17 23:01:11 -0500

    feat: import components from main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartGameComponent } from './start-game/start-game.component';
+import { FormComponent } from './shared/form/form.component';
+import { RoundComponent } from './round/round.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartGameComponent
+    StartGameComponent,
+    FormComponent,
+    RoundComponent
   ],
   imports: [
     BrowserModule,
