239bba9a6a59650dc2ab478e2f26d6d3ca532609
crudazz <>
2018-12-19 18:31:56 -0500

    fix: start game component with BehaivorSubject

--- src/app/start-game/start-game.component.ts
+++ src/app/start-game/start-game.component.ts
@@ -1,20 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { PlayerModel, GameModel } from '../model';
+import { GameService } from '../service/game.service';
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-start-game',
   templateUrl: './start-game.component.html',
   styleUrls: ['./start-game.component.scss']
 })
 export class StartGameComponent implements OnInit {
+  model = new PlayerModel('', 0);  
+  submitted = false;
 
-  constructor(private router: Router) { }
+  game: GameModel;
+  subscription: Subscription;
+
+  constructor(
+    private router: Router,
+    private gameService: GameService
+  ) { }
+
 
   ngOnInit() {
   }
 
-  startGame() {
+  onSubmit() { 
+    this.submitted = true; 
+    this.newGameData(this.model);
     this.router.navigate(['/round']);
+  }
+
+  newGameData(model) {
+    this.game = new GameModel(
+      new PlayerModel(this.model['player1'], 0),
+      new PlayerModel(this.model['player2'], 0),
+      1,
+      ''
+    );
+    this.gameService.setGameData(this.game);
   }
 
 }
